ba706c85a38f5b6b5d70ec12440dc006355032b1
مها بنت البليهد <439003903@pnu.edu.sa>
2021-07-05 15:23:55 +0300

    Add details on manager

---
+++ Pages/Manager/Details.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PermissionSystem.Data;
+using PermissionSystem.Models;
+
+namespace PermissionSystem.Pages.Manager
+{
+    public class Details : PageModel
+    {
+        private readonly PermissionSystem.Data.ApplicationDbContext _context;
+
+        public Details(PermissionSystem.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<LeavePermissionRequest> LeavePermissionRequest { get; set; }
+        public async Task OnGetAsync()
+        {
+            var manager = await _context.Managers.FirstOrDefaultAsync(m => m.Username == User.Identity.Name);
+            LeavePermissionRequest = await _context.LeavePermissionRequests
+                .Include(x => x.Employee)
+                .Include(l => l.RequestReason)
+                .Include(l => l.RequestStatus).Where(m => m.Employee.ManagerId == manager.Id).ToListAsync();
+        }
+    }
+}
