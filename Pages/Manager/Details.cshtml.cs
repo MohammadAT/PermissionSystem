d964a79368b9438568212f91356eac6d25d7a041
مها بنت البليهد <439003903@pnu.edu.sa>
2021-07-06 14:07:56 +0300

    -Add form method -some updates

--- Pages/Manager/Details.cshtml.cs
+++ Pages/Manager/Details.cshtml.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PermissionSystem.Data;
 using PermissionSystem.Models;
@@ -21,8 +23,26 @@ namespace PermissionSystem.Pages.Manager
 
         public LeavePermissionRequest LeavePermissionRequest { get; set; }
 
+        [BindProperty]
+        public InputModel Input { get; set; }
 
-        public async Task OnGetAsync(int? id)
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "Status")]
+            public int StatusId { get; set; }
+
+            [Required]
+            [Display(Name = "Manager Note")]
+            public string ManagerNote { get; set; }
+
+
+
+        }
+
+
+
+        public async Task<IActionResult> OnGetAsync(int? id)
         {
             var manager = await _context.Managers.FirstOrDefaultAsync(m => m.Username == User.Identity.Name);
             LeavePermissionRequest = await _context.LeavePermissionRequests
@@ -30,11 +50,23 @@ namespace PermissionSystem.Pages.Manager
                 .Include(l => l.RequestReason)
                 .Include(l => l.RequestStatus).FirstOrDefaultAsync(m => m.Id == id);
 
+            ViewData["RequestStatus"] = new SelectList(_context.RequestStatuses, "Id", "Name");
+            return Page();
         }
-        public async Task<> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostRejecteAsync(int id)
         {
+            var request = await _context.LeavePermissionRequests.FindAsync(id);
+            if (request != null)
+            {
+                request.RequestStatusId = Input.StatusId;
+                request.ManagerNote = Input.ManagerNote;
+                await _context.SaveChangesAsync();
+            }
 
+            return RedirectToPage();
 
         }
+
     }
 }
+
