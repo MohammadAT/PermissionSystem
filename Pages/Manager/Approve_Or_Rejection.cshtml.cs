48e8559d28864bbf021fca098733c58cca69761b
RanaNaif <RANA_1997@hotmail.com>
2021-07-07 13:50:30 +0300

    .

--- Pages/Manager/Approve_Or_Rejection.cshtml.cs
+++ Pages/Manager/Approve_Or_Rejection.cshtml.cs
@@ -1,5 +1,3 @@
-﻿
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,24 +10,39 @@ using PermissionSystem.Models;
 
 namespace PermissionSystem.Pages.Manager
 {
-    public class IndexModel : PageModel
+    public class Approve_Or_RejectionModel : PageModel
     {
         private readonly PermissionSystem.Data.ApplicationDbContext _context;
 
-        public IndexModel(PermissionSystem.Data.ApplicationDbContext context)
+        public Approve_Or_RejectionModel(PermissionSystem.Data.ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public IList<LeavePermissionRequest> LeavePermissionRequest { get; set; }
-
-        public async Task OnGetAsync()
+        public LeavePermissionRequest LeavePermissionRequest { get; set; }
+        public async Task OnGetAsync(int? id)
         {
             var manager = await _context.Managers.FirstOrDefaultAsync(m => m.Username == User.Identity.Name);
             LeavePermissionRequest = await _context.LeavePermissionRequests
                 .Include(x => x.Employee)
                 .Include(l => l.RequestReason)
-                .Include(l => l.RequestStatus).Where(m => m.Employee.ManagerId == manager.Id).ToListAsync();
+                .Include(l => l.RequestStatus).FirstOrDefaultAsync(m => m.Id == id);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                {
+                    await _context.SaveChangesAsync();
+                }
+
+            }
+
         }
     }
 }
+
+
+
